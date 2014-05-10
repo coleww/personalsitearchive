55d5a4bd920e25718e4f8df5cfdcf97b3c3cd55d
Cole Willsea <coleww@gmail.com>
2014-05-10 12:20:51 -0700

    PACKAGING

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var staticIO = require('node-static');
-var createConductor = require('./lib/conductor.js').createConductor;
 
 var staticServer = new staticIO.Server("./public");
 
 var server = http.createServer(function(request, response) {
   staticServer.serve(request, response);
 });
-
-createConductor(server, 160);
 
 server.listen(8000);
 
