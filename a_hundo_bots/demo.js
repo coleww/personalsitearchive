20076f31829fbc2af86a8cc8b06b97ba544afcd0
Cole Willsea <coleww@gmail.com>
2016-01-10 11:21:59 -0800

    nice

--- a_hundo_bots/demo.js
+++ a_hundo_bots/demo.js
@@ -36,21 +36,6 @@ document.addEventListener('click', function () {
     imgs.innerHTML = ''
     embedded.innerHTML = ''
     title.innerHTML = "THE END (for more fun projects visit <a href='http://www.colewillsea.com'>my web app</a>"
-    var LIL_B_QUOTE = "Words to the wise, you should fear the competition\nBecause I\'m the only vet that\'s a Based God spittin\nAnd you ain\'t in the game until you make a thousand songs\nAnd you dyin\' for this rap, because it\'s the only thing you love\nBirth of Rap"
-    description.textContent = "as Lil B THE BASEDGOD once said in Birth of Rap:"
-    var q = document.createElement('pre')
-    q.textContent = LIL_B_QUOTE
-    imgs.appendChild(q)
-    imgs.appendChild(document.createElement('br'))
-    imgs.appendChild(document.createElement('br'))
-    imgs.appendChild(document.createElement('br'))
-    imgs.appendChild(document.createElement('br'))
-    imgs.appendChild(document.createElement('br'))
-    imgs.appendChild(document.createElement('br'))
-    var s = document.createElement('h3')
-    s.textContent = 'BUT WHAT IS TRUE FOR THE RAP GAME DOES NOT NECESSARILY APPLY TO THE BOTMAKING COMMUNITY. you need not have made a hundred bots or ten or even a single bot to be a part of the botmaking community. you need not publish or share or take credit for every bot you make. you need not feel discouraged when you see others making cool things, for those same people will be there to encourage you when you decide to release yr cool thing! believe me! a year ago today I was feeling seriously down and out cuz i felt like none of the twitter bots I was making were any good and nobody followed me back and everyone else seemed to be churning out these amazing projects. i was bummed, and so i stopped making art, and so i couldn\'t develop my artistic practice either. I dont recall why exactly i started making things again, maybe it was all that pent up emotions inside me that I needed to turn into a series of javascript canvas apps, but i did start making things again and I did find other people who were making cool things that encouraged me and i feel infinitely indebted to them for their support and kind faves and RTs. don\'t fear the competition because there is none! this is not a race/game/content! this is just a bunch of people earnestly sharing their passions with each other 140 characters at a time and with emoji. make stuff cuz you like making it and share it cuz other people will probably like it too and also be sure to always drop hella faves and RTs and also emoji reacts when other people post links to their stuff or pictures of their cats. ~(=^-.-^=) Cole '
-    imgs.appendChild(s)
-
 
     embedded.src = 'https://www.youtube.com/embed/2HQaBWziYvY' + '?autoplay=1'
   }
